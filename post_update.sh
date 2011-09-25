9f59b3b04c37cebc36dea656d2d049dfc5b35224
Steve Ivy <steve@wallrazer.com>
2011-09-24 20:56:28 -0700

    revert path for rake; don't include the full path as it's now on the system path

--- post_update.sh
+++ post_update.sh
@@ -1,10 +1,9 @@
 # post_update script for actions to be taken during a deploy
 
 # generate the site
-echo "building site..."
 export PATH="$PATH:/var/lib/gems/1.8/bin/"
-~/.rvm/scripts/rvm use 1.9.2
-/home/deploy/.rvm/gems/ruby-1.9.2-p290/bin/rake generate
+echo "building site..."
+rake generate
 
 echo "copying public/* to /var/www/stage-monkinetic-com/"
 cp -R public/* /var/www/stage-monkinetic-com/
