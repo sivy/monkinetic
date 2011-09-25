97a85469f2c3e839576f449dffd9d667abf836f3
Steve Ivy <steve@wallrazer.com>
2011-09-24 17:47:48 -0700

    fix log message

--- post_update.sh
+++ post_update.sh
@@ -5,5 +5,5 @@ echo "building site..."
 export PATH="$PATH:/var/lib/gems/1.8/bin/"
 rake generate
 
-echo "copying _site/* to /var/www/stage-monkinetic-com/"
+echo "copying public/* to /var/www/stage-monkinetic-com/"
 cp -R public/* /var/www/stage-monkinetic-com/
